3cfda92545bc71bfd9b1d2033f912afa57b9cd09
Anastazy666 <kamil_sidor@o2.pl>
2020-03-08 17:57:01 +0100

    All the buttons work now (hopefully...). Changed the calculator texture to fit the theme a bit more. Some style tweaks and other stuff are included in this version. The calculator should be fully operational at this point.
    Missing stuff: keyboard support.

--- calculator.js
+++ calculator.js
@@ -40,11 +40,27 @@ function operate(operator, firstArg, secondArg){
     const inputZone = document.getElementById('inputZone');
     const answerZone = document.getElementById('answerZone')
 
+    function checkIfEvil(element){
+        if (element.textContent == '666'){
+            element.classList.add('burning')
+        } else {
+            if (element.classList.contains('burning')){
+                element.classList.remove('burning')
+            }
+        }
+    }
+    function checkIfEvilAll(){
+        checkIfEvil(firstOperandSpan);
+        checkIfEvil(secondOperandSpan);
+        checkIfEvil(answerZone);
+    }
+
     document.querySelectorAll('.digitz').forEach(item => {
         item.addEventListener('click', event => {
             updateOperands(item.textContent)
         })
     })
+
     const firstOperandSpan = document.getElementById('firstOperand');
     const operatorSpan = document.getElementById('operator');
     const secondOperandSpan = document.getElementById('secondOperand');
@@ -52,8 +68,10 @@ function operate(operator, firstArg, secondArg){
     function updateOperands(value){
         if (operatorSpan.textContent == ''){
             firstOperandSpan.textContent += value;
+            checkIfEvilAll();
         } else {
             secondOperandSpan.textContent += value;
+            checkIfEvilAll();
         }
     }
 
@@ -62,29 +80,40 @@ function operate(operator, firstArg, secondArg){
             updateOperator(item.textContent)
         })
     })
+
     function updateOperator(value){
         if (firstOperandSpan.textContent != '' && secondOperandSpan.textContent == ''){
         operatorSpan.textContent = value;
         } else if (firstOperandSpan.textContent != '' && secondOperandSpan.textContent != ''){
-            let meanwhile = operate(operatorSpan.textContent, firstOperandSpan.textContent, secondOperandSpan.textContent);
+            let meanwhile = roundTo5decimalsMax(operate(operatorSpan.textContent, firstOperandSpan.textContent, secondOperandSpan.textContent));
             firstOperandSpan.textContent = meanwhile;
             answerZone.textContent = meanwhile;
             operatorSpan.textContent = value;
+            secondOperandSpan.textContent = '';
+            checkIfEvilAll();
         } else if (firstOperandSpan.textContent == '' && answerZone.textContent != ''){
             firstOperandSpan.textContent = answerZone.textContent;
             operatorSpan.textContent = value;
+            checkIfEvilAll();
         }
      }
 
     const eqlBtn = document.getElementById('equals');
 
+    function roundTo5decimalsMax(halfProduct){
+        halfProduct *= 100000;
+        halfProduct = Math.round(halfProduct)
+        return halfProduct / 100000
+    }
+
     eqlBtn.onclick = function(){
         if (firstOperandSpan.textContent != '' && operatorSpan.textContent != '' && secondOperandSpan.textContent != ''){
-            let answer = operate(operatorSpan.textContent, firstOperandSpan.textContent, secondOperandSpan.textContent);
+            let answer = roundTo5decimalsMax(operate(operatorSpan.textContent, firstOperandSpan.textContent, secondOperandSpan.textContent));
             answerZone.textContent = answer;
             firstOperandSpan.textContent = '';
             operatorSpan.textContent = '';
             secondOperandSpan.textContent = '';
+            checkIfEvilAll();
         }
     }
 
@@ -95,6 +124,7 @@ function operate(operator, firstArg, secondArg){
         operatorSpan.textContent = '';          // of calculations and clearing is writing '.textContent' a kjære-Odin-ton times. Kinda WET.
         secondOperandSpan.textContent = '';
         answerZone.textContent = '';
+        checkIfEvilAll();
     }
 
     const backspaceBtn = document.getElementById('backspace');
@@ -107,5 +137,41 @@ function operate(operator, firstArg, secondArg){
             firstOperandSpan.textContent = firstOperandSpan.textContent.slice(0,-1);
         } else {
             console.log("It seems you are pushing the backspace button regardless of the fact that there's nothing to clear. Hope you enjoy it!")
+        } checkIfEvilAll();
+    }
+
+    const dotBtn = document.getElementById('dot');
+    dotBtn.onclick = function(){
+        if (firstOperandSpan.textContent == ''){
+            firstOperandSpan.textContent += '0.';
+        } else if (firstOperandSpan.textContent != '' && operatorSpan.textContent == ''){
+            let sanityCheck = firstOperandSpan.textContent.split('');
+            if (!sanityCheck.includes('.')){
+                console.log(sanityCheck)
+                firstOperandSpan.textContent += '.';
+            }  
+        } else if (operatorSpan.textContent != '' && secondOperandSpan.textContent == ''){
+            secondOperandSpan.textContent += '0.';
+        } else {
+            let sanityCheck = secondOperandSpan.textContent.split('');
+            if (!sanityCheck.includes('.')) {
+                secondOperandSpan.textContent += '.'
+            }
+        }   checkIfEvilAll();
+    }
+
+    const partyBtn = document.getElementById('vodka');
+    
+    partyBtn.onclick = function(){
+        if (firstOperandSpan.textContent != '' && operatorSpan.textContent != '' && secondOperandSpan.textContent != ''){
+            if (operatorSpan.textContent == '+' || operatorSpan.textContent == '-'){
+            let alc  = parseFloat(firstOperandSpan.textContent) * parseFloat(secondOperandSpan.textContent);
+            secondOperandSpan.textContent = `${alc / 100}`;
+            eqlBtn.click();
+        } else {
+            let alc = parseFloat(secondOperandSpan.textContent) / 100;
+            secondOperandSpan.textContent = `${alc}`;
+            eqlBtn.click();
         }
+        } checkIfEvilAll();
     }
