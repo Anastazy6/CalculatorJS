128130103f98f3a54218fcce2de3df3b09ca41e3
Anastazy666 <kamil_sidor@o2.pl>
2020-03-08 03:56:48 +0100

    Added the most important JS: all four basic operations work, chaining works and 'equals' button works. 'Clear' button works too, though backspace, % and dot do nothing at the moment. Tweaked styles a bit, % button is no longer frost themed (reserved for operators) and shares fel theme with 'equals' button so as to signal that it's going to function similarilly: if I don't change my mind it's gonna be just like the 'equals' button that treats the second operand as a given percentage of the first operand.

--- calculator.js
+++ calculator.js
@@ -1,40 +1,93 @@
 function add(a,b){
-    return a + b
+    return parseFloat(a) + parseFloat(b)
 }
 function subtract(a,b){
-    return a - b
+    return parseFloat(a) - parseFloat(b)
 }
 function multiply(a,b){
-    return a * b
+    return parseFloat(a) * parseFloat(b)
 }
 function divide(a,b){
-    return a / b
+    return parseFloat(a) / parseFloat(b)
 }
-function operator(operator, firstArg, secondArg){
+
+function operate(operator, firstArg, secondArg){
     switch (operator){
-        case add:
+        case '+':
             return add(firstArg,secondArg);
             break;
-        case subtract:
+        case '-':
             return subtract(firstArg,secondArg);
             break;
-        case multiply:
+        case 'x':
             return multiply(firstArg,secondArg);
             break;
-        case divide:
+        case '*':
+            return multiply(firstArg,secondArg);
+        case '/':
             return divide(firstArg,secondArg);
             break;
-    }
+        }
     }
 
     const calculatorZone = document.getElementById('calculator');
     const displayZone = document.getElementById('display');
     const inputZone = document.getElementById('inputZone');
     const answerZone = document.getElementById('answerZone')
-   // displayZone.style.width = parseInt(calculatorZone.style.width.slice(0,-2) * 0.9) + "px";
-  //  inputZone.style.width = displayZone.style.width;
-   // answerZone.style.width = displayZone.style.width;
-  //  displayZone.style.zIndex = calculatorZone.style.zIndex + 1;
-    //const equalsBtn = document.getElementById('equals')
 
-  
+    document.querySelectorAll('.digitz').forEach(item => {
+        item.addEventListener('click', event => {
+            updateOperands(item.textContent)
+        })
+    })
+    const firstOperandSpan = document.getElementById('firstOperand');
+    const operatorSpan = document.getElementById('operator');
+    const secondOperandSpan = document.getElementById('secondOperand');
+
+    function updateOperands(value){
+        if (operatorSpan.textContent == ''){
+            firstOperandSpan.textContent += value;
+        } else {
+            secondOperandSpan.textContent += value;
+        }
+    }
+
+    document.querySelectorAll('.operatorBtn').forEach(item => {
+        item.addEventListener('click', event => {
+            updateOperator(item.textContent)
+        })
+    })
+    function updateOperator(value){
+        if (firstOperandSpan.textContent != '' && secondOperandSpan.textContent == ''){
+        operatorSpan.textContent = value;
+        } else if (firstOperandSpan.textContent != '' && secondOperandSpan.textContent != ''){
+            let meanwhile = operate(operatorSpan.textContent, firstOperandSpan.textContent, secondOperandSpan.textContent);
+            firstOperandSpan.textContent = meanwhile;
+            answerZone.textContent = meanwhile;
+            operatorSpan.textContent = value;
+        } else if (firstOperandSpan.textContent == '' && answerZone.textContent != ''){
+            firstOperandSpan.textContent = answerZone.textContent;
+            operatorSpan.textContent = value;
+        }
+     }
+
+    const eqlBtn = document.getElementById('equals');
+
+    eqlBtn.onclick = function(){
+        if (firstOperandSpan.textContent != '' && operatorSpan.textContent != '' && secondOperandSpan.textContent != ''){
+            let answer = operate(operatorSpan.textContent, firstOperandSpan.textContent, secondOperandSpan.textContent);
+            answerZone.textContent = answer;
+            firstOperandSpan.textContent = '';
+            operatorSpan.textContent = '';
+            secondOperandSpan.textContent = '';
+        }
+    }
+
+    const clearBtn = document.getElementById('clear');
+
+    clearBtn.onclick = function(){
+        firstOperandSpan.textContent = '';
+        operatorSpan.textContent = '';
+        secondOperandSpan.textContent = '';
+        answerZone.textContent = '';
+    }
